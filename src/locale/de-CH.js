ad8d52c79fb23d5887c754c1681eaa87b1e5f0ae
LOTHALLOC\fs <f.schaper@lothal.ch>
2020-03-20 09:01:45 +0100

    Added german locale

--- src/locale/de-CH.js
+++ src/locale/de-CH.js
@@ -1,5 +1,26 @@
 const deCh = {
-    justNow: 'gerade eben'
+    one: 'eins',
+    two: 'zwei',
+    three: 'drei',
+    four: 'vier',
+    five: 'fünf',
+    six: 'sechs',
+    seven: 'sieben',
+    eight: 'acht',
+    nine: 'neun',
+    justNow: 'gerade eben',
+    minuteAgo: 'vor einer Minute',
+    minutesAgo: 'vor $ Minuten',
+    hourAgo: 'vor einer Stunde',
+    hoursAgo: 'vor $ Stunden',
+    yesterday: 'gestern',
+    daysAgo: 'vor $ Tagen',
+    lastWeek: 'letzte Woche',
+    weeksAgo: 'vor $ Wochen',
+    lastMonth: 'letzten Monat',
+    monthsAgo: 'vor $ Monaten',
+    lastYear: 'letztes Jahr',
+    yearsAgo: 'vor $ Jahren'
 }
 
 export default deCh;
